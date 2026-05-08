19644ea6dc7fe9d80a344d7ff404201e62182b01
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-08 15:45:55 -0500

    Added: Require methods added to UserInterface.java

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class UserInterface {
+
+    public void display(){
+
+    }
+
+    private void init(){
+
+    }
+
+    public void displayVehicles(ArrayList<String> list){
+
+    }
+
+    public void processAllVehiclesRequest(){
+
+    }
+
+    public void processAddVehicleRequest(){
+
+    }
+
+    public void processRemoveVehicleRequest(){
+
+    }
+
+    public void processGetByPrice(){
+
+    }
 }
