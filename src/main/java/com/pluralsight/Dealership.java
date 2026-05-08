f00969fb7a97a9109d9e987917ce988b37179301
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-08 15:51:48 -0500

    Added: Require methods added to Dealership.java

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,60 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<String> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone, ArrayList<String> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+    }
+
+    public ArrayList<String> getVehicleByPrice(int min, int max){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getVehiclesByMakeModel(){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getVehiclesByYear(){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getVehicleByColor(int min, int max){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getVehiclesByMileage(){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getVehiclesByType(){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public ArrayList<String> getAllVehicles(int min, int max){
+        ArrayList<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public void addVehicle(){
+
+    }
+
+    public void removeVehicle(){
+
+    }
 }
