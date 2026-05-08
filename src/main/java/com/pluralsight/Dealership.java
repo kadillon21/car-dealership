6b871c5a0bb324a671199462e4c6b69be657bc71
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-08 15:52:53 -0500

    Added: Added getters and setters for objects createdin Dealership.java

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -15,6 +15,30 @@ public class Dealership {
         this.inventory = inventory;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public ArrayList<String> getVehicleByPrice(int min, int max){
         ArrayList<String> results = new ArrayList<>();
         return results;
