5fac989e77be8900cea738979cb5c46672dcd5eb
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-10 21:46:36 -0500

    Update: Changed all methods to static methods so they can be accessed by main method in Program.java

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -4,31 +4,43 @@ import java.util.ArrayList;
 
 public class UserInterface {
 
-    public void display(){
+    public static void display(){
+
+        System.out.println("Dealership Search Engine");
+        System.out.println("1) Find by price range");
+        System.out.println("2) Find by make / model");
+        System.out.println("3) Find by year range");
+        System.out.println("4) Find by color");
+        System.out.println("5) Find by mileage range");
+        System.out.println("6) Find by vehicle type");
+        System.out.println("7) List ALL vehicles");
+        System.out.println("8) Add a vehicle");
+        System.out.println("9) Remove a vehicle");
+        System.out.println("10) Quit");
 
     }
 
-    private void init(){
+    private static void init(){
 
     }
 
-    public void displayVehicles(ArrayList<String> list){
+    public static void displayVehicles(ArrayList<String> list){
 
     }
 
-    public void processAllVehiclesRequest(){
+    public static void processAllVehiclesRequest(){
 
     }
 
-    public void processAddVehicleRequest(){
+    public static void processAddVehicleRequest(){
 
     }
 
-    public void processRemoveVehicleRequest(){
+    public static void processRemoveVehicleRequest(){
 
     }
 
-    public void processGetByPrice(){
+    public static void processGetByPrice(){
 
     }
 }
