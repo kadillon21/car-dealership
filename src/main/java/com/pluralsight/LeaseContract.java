b29e16f7b0415075001fcf16992bc6fc65e8f91d
kadillon21 <kadillon21@gmail.com>
2026-05-15 15:44:08 -0500

    Refactored `LeaseContract` class (Accidently coded SalesContract.java in this file)

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,45 +1,23 @@
 package com.pluralsight;
 
 public class LeaseContract extends Contract{
-    double price = vehicleSold.getPrice();
-    double salesTaxAmount = price * 0.05;
-    int recordingFee = 100;
-    int processingFee;
-    boolean financeOption = true;
 
+    private double expectedEndingValue = .5;
+    private double leaseFee = .07;
+
+    public LeaseContract(double expectedEndingValue, double leaseFee) {
+        this.expectedEndingValue = expectedEndingValue;
+        this.leaseFee = leaseFee;
+    }
 
     @Override
     public double getTotalPrice() {
-
-        if (price < 100000) {
-            processingFee = 295;
-        } else {
-            processingFee = 495;
-        }
-
-        return price + salesTaxAmount + recordingFee + processingFee;
+        double endingValue = expectedEndingValue * vehicleSold.getPrice();
+        return endingValue + leaseFee;
     }
 
     @Override
     public double getMonthlyPayment() {
-        double monthlyInterestRate;
-        int paymentTerm;
-
-        if (financeOption){
-            if (getTotalPrice() >= 10000) {
-                monthlyInterestRate = 0.0425 / 12;
-                paymentTerm = 48;
-            } else {
-                monthlyInterestRate = 0.0525 / 12;
-                paymentTerm = 24;
-            }
-
-            return getTotalPrice() *
-                    (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, paymentTerm)) /
-                    (Math.pow(1 + monthlyInterestRate, paymentTerm) - 1);
-        } else {
-            return 0;
-        }
-
+        return 0.04 / 36;
     }
 }
