78ca6ded1bebfb5a682b04a353911852f45e4b7e
kadillon21 <kadillon21@gmail.com>
2026-05-15 15:00:35 -0500

    Refactored `Contract` to abstract class and added core properties with abstract methods.

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+
+    protected String date;
+    protected String customerName;
+    protected String customEmail;
+    protected Vehicle vehicleSold;
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomEmail() {
+        return customEmail;
+    }
+
+    public void setCustomEmail(String customEmail) {
+        this.customEmail = customEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
 }
